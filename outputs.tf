77a8668022d6dbeccabcc68e9fcb90575e8cb483
Casey Boettcher <chb@socketbox.com>
2023-11-30 16:31:42 +0000

    Add role name to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "arn" {
   description = "ARN of the IAM role"
   value       = aws_iam_role.role.arn
 }
+
+output "name" {
+  description = "Name of the IAM role"
+  value       = aws_iam_role.role.name
+}
